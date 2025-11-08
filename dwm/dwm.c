202fb003abde8b3eff37d870e0f0bb638ebd6b2c
b_chirva <bchirva@gmail.com>
2025-11-09 00:43:44 +0500

    dwm: remove layout indicator

--- dwm/dwm.c
+++ dwm/dwm.c
@@ -760,10 +760,7 @@ drawbar(Monitor *m)
 		drw_text(drw, x, 0, w, bh, lrpad / 2, icon, urg & 1 << i);
 		x += w;
 	}
-	w = TEXTW(m->ltsymbol);
-	drw_setscheme(drw, scheme[SchemeNorm]);
-	x = drw_text(drw, x, 0, w, bh, lrpad / 2, m->ltsymbol, 0);
-
+	
 	if ((w = m->ww - tw - x) > bh) {
 		if (m->sel) {
 			drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
